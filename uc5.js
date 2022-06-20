c1f4fecd9450ef212752556eace1f6c9a5aad87b
ovi2410 <101970733+ovi2410@users.noreply.github.com>
2022-06-20 09:44:18 +0530

    delete element using the first name

---
+++ uc5.js
@@ -0,0 +1,7 @@
+function DeleteAddress(addressbook){
+    let resultAddress = SearchName(addressbook);
+    var index= addressArr.indexOf(resultAddress);
+    return index;
+}
+let deleteIndex=addressArr.find(DeleteAddress);
+console.log(addressArr.splice(deleteIndex,1)+" is deleted")
