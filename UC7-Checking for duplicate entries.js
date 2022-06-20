8511873370a8c2f24cd88265685ea7ab35c045b7
ovi2410 <101970733+ovi2410@users.noreply.github.com>
2022-06-20 09:46:01 +0530

    checking for duplicate entries

---
+++ UC7-Checking for duplicate entries.js
@@ -0,0 +1,16 @@
+function CheckDuplicate(a)
+{
+    if(a.firstName.includes("Punit"))
+    ++n;
+return n;
+}
+let count = addressArr.filter(CheckDuplicate);
+if(count=1)
+{
+    console.log("There is no duplicate entry for the name");
+}
+else if(count>1)
+{
+    console.log("There is duplicate entry for the given name")
+
+}
