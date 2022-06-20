c89c4f2b3726d507dbb4b636e26bffb7d58bdaf4
ovi2410 <guneshwarifegade@gmail.com>
2022-06-20 09:26:11 +0530

    getting count of people living city or state

--- uc9.js
+++
@@ -1,7 +0,0 @@
-function ReturnCityMap(a)
-{
-    return a.city+ "->"+a.firstName;
-}
-let addressCityMap= addressArr.map(ReturnCityMap);
-console.log("Viewing the people by their city")
-console.log(addressCityMap);
